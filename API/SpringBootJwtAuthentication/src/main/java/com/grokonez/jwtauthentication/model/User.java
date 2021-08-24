6d0a93048c90a879dbdcc47c78c7172ad35d41a7
Donia-Hamadaa <85892086+Donia-Hamadaa@users.noreply.github.com>
2021-08-24 15:56:39 +0100

    affichage users

--- API/SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/model/User.java
+++ API/SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/model/User.java
@@ -56,13 +56,11 @@ public class User{
     private Status status ;
 
 
-
-
-
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles", 
     	joinColumns = @JoinColumn(name = "user_id"), 
     	inverseJoinColumns = @JoinColumn(name = "role_id"))
+
     private Set<Role> roles = new HashSet<>();
 
     public User() {}
