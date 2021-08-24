2fdcd9004c735ce1abafa6d72c711d65a4e9f70c
Donia-Hamadaa <85892086+Donia-Hamadaa@users.noreply.github.com>
2021-08-24 21:33:53 +0100

    affichage users

---
+++ WEB/src/app/user/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    username: string;
+    phone: string;
+    cin: string;
+    password: string;
+    status: string;
+
+  }
