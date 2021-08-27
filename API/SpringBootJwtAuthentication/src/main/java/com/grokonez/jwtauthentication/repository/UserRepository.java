b26328abbb86f122050dd87ac45656046149815f
amine <mohamedamine.berriche@edu.isetcom.tn>
2021-08-27 10:50:16 +0100

    up

--- API/SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/repository/UserRepository.java
+++ API/SpringBootJwtAuthentication/src/main/java/com/grokonez/jwtauthentication/repository/UserRepository.java
@@ -1,15 +1,28 @@
 package com.grokonez.jwtauthentication.repository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.grokonez.jwtauthentication.model.User;
+import com.grokonez.jwtauthentication.model.*;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
     Optional<User> findByUsername(String username);
+
+    //List<User> findByRole(RoleName role);
+
+   List<User> findByRoles_Name(RoleName name);
+
+    //static List<User> findByRoles_Name(RoleName name) {
+
+       // return null;
+    //}
+
+
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
--- API/src/main/java/com/grokonez/jwtauthentication/model/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.grokonez.jwtauthentication.model;
-
-public enum  RoleName {
-    ROLE_USER,
-    ROLE_ENCADRANT,
-    ROLE_HR
-}
